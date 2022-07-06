b226f1951fb951a65adf9ce3e2eee5b6d0595594
RaulHinojosa24 <raul.hinojosa.perez@gmail.com>
2022-07-06 13:35:47 +0200

    crud get/post/put usuarios

--- src/main/java/com/example/demo/controller/Controlador.java
+++ src/main/java/com/example/demo/controller/Controlador.java
@@ -1,10 +1,14 @@
 package com.example.demo.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,8 +21,41 @@ public class Controlador {
 	@Autowired
 	UsuariosDAO usuariosDAO;
 	
-	@GetMapping("/")
-	public ResponseEntity<List<Usuarios>> listShops() {
+	@GetMapping("/usuarios/{id}")
+	public ResponseEntity<Optional<Usuarios>> findUser(@PathVariable int id) {
+		return ResponseEntity.ok(usuariosDAO.findById(id));
+	}
+
+	@GetMapping("/usuarios/")
+	public ResponseEntity<List<Usuarios>> listUsers() {
 		return ResponseEntity.ok(usuariosDAO.findAll());
+	}
+
+	@PostMapping("/usuarios/")
+	public ResponseEntity<Usuarios> createUser(Usuarios usuario) {
+		try {
+			if (usuariosDAO.existsById(usuario.getId())) {
+				return ResponseEntity.notFound().build();
+			}
+
+			usuariosDAO.save(usuario);
+			return ResponseEntity.ok(usuario);
+		} catch (Exception e) {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@PutMapping("/usuarios/")
+	public ResponseEntity<Usuarios> updateUser(Usuarios usuario) {
+		try {
+			if (!usuariosDAO.existsById(usuario.getId())) {
+				return ResponseEntity.notFound().build();
+			}
+
+			usuariosDAO.save(usuario);
+			return ResponseEntity.ok(usuario);
+		} catch (Exception e) {
+			return ResponseEntity.notFound().build();
+		}
 	}
 }
