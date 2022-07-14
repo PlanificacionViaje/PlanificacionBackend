e86ed7cfb6354353895d8f681edd9a38b1921b51
RaulHinojosa24 <raul.hinojosa.perez@gmail.com>
2022-07-14 10:57:58 +0200

    Añadido schema.sql corregido y data.sql con datos

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -1,6 +1,34 @@
-INSERT INTO `usuarios` (`nombre`, `correo`, `contrasena`, `apellidos`) VALUES
-('lila', 'lila@gmail.com', 'lila', 'sarfson'),
-('admi', 'admin@gmail.com', '1234', 'admin');
+INSERT INTO `usuarios` (`idUsuario`, `nombre`, `apellidos`, `correo`, `contrasena`) VALUES
+('1', 'Raul', 'Hinojosa Perez', 'raul@gmail.com', 'qwerty1234'),
+('2', 'Lila', 'Sarfson Gutierrez', 'lila@gmail.com', 'asdf*'),
+('3', 'Arantxa', 'Delgado Porras', 'arantxa@gmail.com', 'ara123#'),
+('4', 'Lidia', 'Alvarez Vaello', 'lidia@gmail.com', 'pass1234'),
+('5', 'Toni', 'Serrano Jimenez', 'toni@gmail.com', 'toni123qwe'),
+('6', 'Almudena', 'Raya Alcaide', 'arantxa@gmail.com', 'ara123#'),
+('7', 'Miriam', 'Feijoo Lorden', 'miriam@gmail.com', 'Aqwer12');
 
-INSERT INTO `viajes` (`idViaje`, `nombre`, `descripcion`, `fechaInicio`, `fechaFin`, `Usuarios_idUsuario`) VALUES (NULL, 'Londres', 'ciudad de la lluvia', '2022-08-01', '2022-09-01', '1');
+INSERT INTO `viajes` (`idViaje`, `nombre`, `descripcion`, `fechaInicio`, `fechaFin`, `Usuarios_idUsuario`) VALUES
+('1', 'Tenerife', 'Por fin... Una semanita en las Canarias ;-;', '2022-08-12', '2022-08-19', '1'),
+('2', 'Paises Bajos', 'Amsterdam.', '2022-11-08', '2022-11-12', '3'),
+('3', 'Islandia', 'Viaje de ensueño con mi pareja, una semanita llena de aventuras!', '2023-03-08', '2023-03-20', '7'),
+('4', 'Puerto Rico', '1 mes de buena vida ;)', '2023-06-02', '2023-07-05', '4'),
+('5', 'Francia', 'Baguette.', '2022-09-16', '2022-09-22', '4'),
+('6', 'Around the world!', 'Ruta mochilera', '2022-07-29', '2023-09-09', '5'),
+('7', 'Rusia', 'A pasar frio y a beber vodka, blyat!', '2022-11-30', '2022-12-08', '6'),
+('8', 'USA', 'Espero no morir en un super tiroteado jej....', '2022-12-29', '2023-01-05', '2');
 
+INSERT INTO `itemsviaje` (`idItemsViaje`, `nombre`, `descripcion`, `hora`, `precio`, `ubicacionLatitud`, `ubicacionLongitud`, `Viajes_IdViaje`) VALUES
+('1', 'Guigui', 'Playa preciosa de arena negra', '16:00:00', '0', '28.302463', '-16.596816', '1'),
+('2', 'Santa Cruz de Tenerife', 'Capital de tenerife', '10:00:00', '0', '28.302463', '-16.596816', '1'),
+('3', 'Amsterdam', 'If you know you know...', '10:00:00', '0', '52.397401', '4.975370', '2'),
+('4', 'Eindhoven', 'Pa hacer algo más que...', '10:00:00', '0', '52.397401', '4.975370', '2'),
+('5', 'Reikiavik', '', '10:00:00', '0', '64.731076', '-18.343358', '3'),
+('6', 'Parque nacional Vatnajökull', '', '16:00:00', '0', '64.731076', '-18.343358', '3'),
+('7', 'San Juan', '', '20:00:00', '0', '18.238976', '-66.465175', '4'),
+('8', 'Ponce', '', '12:00:00', '0', '18.238976', '-66.465175', '4'),
+('9', 'Torre Eiffel', 'La torre más famosa de Francia (wow)', '16:00:00', '0', '48.857601', '2.293286', '5'),
+('10', 'Petra, Jordania', 'Ciudad de Petra, una maravilla del mundo', '11:30:00', '0', '30.327954', '35.444448', '6'),
+('11', 'Moscu', 'Frio', null, '0', '55.761484', '38.613550', '7'),
+('12', 'San Petersburgo', 'Frio x2', null, '0', '59.849709', '30.534222', '7'),
+('13', 'San Francisco', '', null, '0', '37.786733', '-122.353460', '8'),
+('14', 'Miami', '', null, '0', '25.755169', '-80.214522', '8');
---
+++ target/classes/schema.sql
@@ -0,0 +1,67 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+DROP DATABASE IF EXISTS `freeway`;
+
+-- -----------------------------------------------------
+-- Schema freeway
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `freeway` DEFAULT CHARACTER SET utf8 ;
+USE `freeway` ;
+
+-- -----------------------------------------------------
+-- Table `freeway`.`Usuarios`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `freeway`.`Usuarios` (
+  `idUsuario` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(45) NOT NULL,
+  `apellidos` VARCHAR(45) NOT NULL,
+  `correo` VARCHAR(45) NOT NULL,
+  `contrasena` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idUsuario`))
+ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `freeway`.`Viajes`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `freeway`.`Viajes` (
+  `idViaje` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(45) NOT NULL,
+  `descripcion` VARCHAR(500) NULL,
+  `fechaInicio` DATE NOT NULL,
+  `fechaFin` DATE NOT NULL,
+  `Usuarios_idUsuario` INT NOT NULL,
+  PRIMARY KEY (`idViaje`),
+  INDEX `fk_Viajes_Usuario_idx` (`Usuarios_idUsuario` ASC),
+  CONSTRAINT `fk_Viajes_Usuarios`
+    FOREIGN KEY (`Usuarios_idUsuario`)
+    REFERENCES `freeway`.`Usuarios` (`idUsuario`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `freeway`.`itemsViaje`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `freeway`.`ItemsViaje` (
+  `idItemsViaje` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(45) NOT NULL,
+  `descripcion` VARCHAR(100) NULL,
+  `hora` TIME NULL,
+  `precio` DOUBLE NULL,
+  `ubicacionLatitud` DOUBLE NOT NULL,
+  `ubicacionLongitud` DOUBLE NOT NULL,
+  `Viajes_idViaje` INT NOT NULL,
+  PRIMARY KEY (`idItemsViaje`),
+  INDEX `fk_ItemsViaje_Viajes_idx` (`Viajes_idViaje` ASC) ,
+  CONSTRAINT `fk_ItemsViaje_Viajes`
+    FOREIGN KEY (`Viajes_idViaje`)
+    REFERENCES `freeway`.`Viajes` (`idViaje`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
